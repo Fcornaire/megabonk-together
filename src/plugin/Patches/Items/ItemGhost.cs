01677de82e7b64763c3ed7e5ed06b4c842eed12d
FCornaire <cornairemax@yahoo.fr>
2026-01-27 23:39:07 +0100

    fix: item ghost crashing on shrine interactions

--- src/plugin/Patches/Items/ItemGhost.cs
+++ src/plugin/Patches/Items/ItemGhost.cs
@@ -11,12 +11,24 @@ namespace MegabonkTogether.Patches.Items
         private static readonly ISynchronizationService synchronizationService = Plugin.Services.GetService<ISynchronizationService>();
         private static readonly IPlayerManagerService playerManagerService = Plugin.Services.GetService<IPlayerManagerService>();
 
+        /// <summary>
+        /// Skip original interaction if not successful. This prevent a memory access violation somehow when the original code run 
+        /// </summary>
         [HarmonyPrefix]
         [HarmonyPatch(nameof(ItemGhost.OnInteracted))]
-        public static void OnInteracted_Prefix(ItemGhost __instance) //TODO: handle ghost item interaction  
+        public static bool OnInteracted_Prefix(bool success)
         {
-            //var isRemoteItem = playerManagerService.IsRemoteItem(__instance);
+            if (!synchronizationService.HasNetplaySessionStarted())
+            {
+                return true;
+            }
 
+            if (!success)
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
