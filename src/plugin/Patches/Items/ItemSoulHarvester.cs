e9c0715bb5fd506a5277dd4c6d28b14adff52b9f
FCornaire <cornairemax@yahoo.fr>
2026-03-08 00:31:06 +0100

    feat: prevent triggering effects for unowned tomes

---
+++ src/plugin/Patches/Items/ItemSoulHarvester.cs
@@ -0,0 +1,35 @@
+﻿using Assets.Scripts.Actors.Enemies;
+using Assets.Scripts.Inventory__Items__Pickups.Items.ItemImplementations;
+using HarmonyLib;
+using MegabonkTogether.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MegabonkTogether.Patches.Items
+{
+    [HarmonyPatch(typeof(ItemSoulHarvester))]
+    internal static class ItemSoulHarvesterPatches
+    {
+        private static readonly ISynchronizationService synchronizationService = Plugin.Services.GetService<ISynchronizationService>();
+        private static readonly IPlayerManagerService playerManagerService = Plugin.Services.GetService<IPlayerManagerService>();
+
+        /// <summary>
+        /// Prevent triggering SoulHarvester effect if not owned by the local player.
+        /// </summary>
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(ItemSoulHarvester.OnEnemyDied))]
+        public static bool OnEnemyDied_Prefix(ItemSoulHarvester __instance, Enemy enemy)
+        {
+            if (!synchronizationService.HasNetplaySessionStarted())
+            {
+                return true;
+            }
+
+            if (GameManager.Instance.player.inventory.itemInventory.GetItem(Assets.Scripts.Inventory__Items__Pickups.Items.EItem.SoulHarvester) != __instance)
+            {
+                return false;
+            }
+            
+            return true;
+        }
+    }
+}
---
+++ src/plugin/Patches/Items/TomeUtility.cs
@@ -0,0 +1,35 @@
+﻿using Assets.Scripts.Inventory__Items__Pickups;
+using HarmonyLib;
+using MegabonkTogether.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MegabonkTogether.Patches.Items
+{
+    [HarmonyPatch(typeof(TomeUtility))]
+    internal static class TomeUtilityPatches
+    {
+        private static readonly ISynchronizationService synchronizationService = Plugin.Services.GetService<ISynchronizationService>();
+
+        /// <summary>
+        /// Prevent triggering tome effect if the tome is added by a netplayer. (Chaos tome)
+        /// </summary>
+        /// <returns></returns>
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(TomeUtility.CheckSpecialTomes))]
+        public static bool CheckSpecialTomes_Prefix()
+        {
+            if (!synchronizationService.HasNetplaySessionStarted())
+            {
+                return true;
+            }
+
+            if (Plugin.Instance.IS_NETPLAYER_ADDING_TOME)
+            {
+                Plugin.Instance.IS_NETPLAYER_ADDING_TOME = false;
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
